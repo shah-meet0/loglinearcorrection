711294ab8072a024295025a46c79b9781658c43d
shah-meet0 <meetsamshah@gmail.com>
2025-03-12 20:17:16 +0000

    added results on different types of errors

--- tests/continous_correction.py
+++ tests/continous_correction.py
@@ -1,17 +1,19 @@
 from utils.data_generating_processes import DGP, MVNDataGenerator, ConstantGenerator, CombinedDataGenerator, IndependentNormErrorGenerator
 from utils.dependence_funcs import independent_absolute, constant_mean, independent_squared
 import statsmodels.api as sm
+from statsmodels.nonparametric.kernel_regression import KernelReg
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.neural_network import MLPRegressor
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import PolynomialFeatures
 import pandas as pd
 
-# THIS MODULE IS FOR PRELIMNARY TESTING OF THE CONTINUOUS CORRECTION ALGORITHM
+# THIS MODULE IS FOR PRELIMINARY TESTING OF THE CONTINUOUS CORRECTION ALGORITHM
 # THIS WILL PROBABLY BE DELETED IN A FUTURE PATCH, AS MORE FUNCTIONS WILL DEPRECATE ITS QUALITY
 
 x_generator = MVNDataGenerator(means=[0], sigma=[[1]])
@@ -23,13 +25,13 @@ betas = np.array([2, 1])
 
 dgp = DGP(combined_generator, betas, error_generator, exponential=True)
 
-x,y,u = dgp.generate(50000)
+x,y,u = dgp.generate(10000)
 
-# Model dependent percentage changes
-
-example_x = np.linspace(-3, 3, 7)
-example_y = np.exp(2*example_x + 1) * np.exp(0.5 * np.abs(example_x)**2)
-percentage_changes = example_y[1:] / example_y[:-1] - 1
+# # Model dependent percentage changes
+#
+# example_x = np.linspace(-3, 3, 7)
+# example_y = np.exp(2*example_x + 1) * np.exp(0.5 * np.abs(example_x)**2)
+# percentage_changes = example_y[1:] / example_y[:-1] - 1
 
 entries = []
 
@@ -52,19 +54,44 @@ target = np.exp(residuals)
 input = x
 
 validation_input = sm.tools.add_constant(np.linspace(-1.5, 1.5, 1000).reshape(-1, 1), prepend=False)
-validation_target = np.exp(np.abs(validation_input[:, 0])**2/2)
+validation_target = np.exp(np.abs(validation_input[:, 0]) ** 2/2)
 
-# Linear Regression
-model_OLS = LinearRegression()
-model_OLS.fit(input, target)
-preds_ols = model_OLS.predict(validation_input)
+# Simple OLS
+model_baseline = LinearRegression()
+model_baseline.fit(input, target)
+preds_baseline = model_baseline.predict(validation_input)
 
-mse_ols = mean_squared_error(validation_target, preds_ols)
-r2_ols = r2_score(validation_target, preds_ols)
-print("Linear Regression")
+mse_baseline = mean_squared_error(validation_target, preds_baseline)
+r2_baseline = r2_score(validation_target, preds_baseline)
+print("Baseline")
+print(f"MSE: {mse_baseline:.3f}")
+print(f"R2: {r2_baseline:.3f}")
+entries.append(["Baseline", mse_baseline, r2_baseline])
+
+
+# Polynomial Regression
+model_ols_polynomial = LinearRegression()
+poly_features = PolynomialFeatures(5, include_bias=False)
+model_ols_polynomial.fit(poly_features.fit_transform(input), target)
+preds_ols_polynomial = model_ols_polynomial.predict(poly_features.transform(validation_input))
+
+mse_ols = mean_squared_error(validation_target, preds_ols_polynomial)
+r2_ols = r2_score(validation_target, preds_ols_polynomial)
+print("Polynomial OLS")
 print(f"MSE: {mse_ols:.3f}")
 print(f"R2: {r2_ols:.3f}")
-entries.append(["OLS", mse_ols, r2_ols])
+entries.append(["Polynomial OLS", mse_ols, r2_ols])
+
+# Kernel Regression
+model_kr = KernelReg(target, input[:,0], var_type='c')
+preds_kr = model_kr.fit(validation_input[:,0])[0]
+
+mse_kr = mean_squared_error(validation_target, preds_kr)
+r2_kr = r2_score(validation_target, preds_kr)
+print("Kernel Regression")
+print(f"MSE: {mse_kr:.3f}")
+print(f"R2: {r2_kr:.3f}")
+entries.append(["KR", mse_kr, r2_kr])
 
 # Random Forest
 model_RF = RandomForestRegressor()
@@ -100,9 +127,9 @@ grid_search_gb = GridSearchCV(model_GB, param_grid_gb, cv=5, n_jobs=-1)
 grid_search_gb.fit(input, target)
 preds_gb = grid_search_gb.predict(validation_input)
 
-
 mse_gb = mean_squared_error(validation_target, preds_gb)
 r2_gb = r2_score(validation_target, preds_gb)
+
 
 print("Gradient Boosting")
 print(f"MSE: {mse_gb:.3f}")
@@ -136,24 +163,28 @@ entries.append(["NN", mse_nn, r2_nn])
 
 
 df = pd.DataFrame(entries, columns=["Model", "MSE", "R2"])
-df.to_latex()
+df.to_csv('./results/continuous_correction_independent_squared.csv')
 
 
 # PLOT RESULTS
 
-fig, ax = plt.subplots(2,2, sharey=True, sharex=True)
+fig, ax = plt.subplots(2,3, sharey=True, sharex=True, figsize=(10, 9))
 for axis in ax.flatten():
     sns.lineplot(x=validation_input[:, 0], y=validation_target, ax=axis, label='True', color='green')
 
-sns.lineplot(x=validation_input[:, 0], y=preds_ols, ax=ax[0][0], label='OLS', color='red')
-sns.lineplot(x=validation_input[:, 0], y=preds_rf, ax=ax[0][1], label='RF', color='red')
-sns.lineplot(x=validation_input[:, 0], y=preds_gb, ax=ax[1][0], label='GB', color='red')
-sns.lineplot(x=validation_input[:, 0], y=preds_nn, ax=ax[1][1], label='NN', color='red')
-ax[0][0].set_title(f'OLS, MSE: {mse_ols:.3f}')
-ax[0][1].set_title(f'Random Forest, MSE: {mse_rf:.3f}')
-ax[1][0].set_title(f'Gradient Boosting, MSE: {mse_gb:.3f}')
-ax[1][1].set_title(f'Neural Network, MSE: {mse_nn:.3f}')
+sns.lineplot(x=validation_input[:, 0], y=preds_baseline, ax=ax[0][0], label='OLS', color='red')
+sns.lineplot(x=validation_input[:, 0], y=preds_ols_polynomial, ax=ax[0][1], label='Polynomial OLS', color='red')
+sns.lineplot(x=validation_input[:, 0], y=preds_kr, ax=ax[0][2], label='Kernel', color='red')
+sns.lineplot(x=validation_input[:, 0], y=preds_rf, ax=ax[1][0], label='RF', color='red')
+sns.lineplot(x=validation_input[:, 0], y=preds_gb, ax=ax[1][1], label='GB', color='red')
+sns.lineplot(x=validation_input[:, 0], y=preds_nn, ax=ax[1][2], label='NN', color='red')
+ax[0][0].set_title(f'OLS, MSE: {mse_baseline:.3f}')
+ax[0][1].set_title(f'Polynomial OLS, MSE: {mse_ols:.3f}')
+ax[0][2].set_title(f'Kernel, MSE: {mse_kr:.3f}')
+ax[1][0].set_title(f'Random Forest, MSE: {mse_rf:.3f}')
+ax[1][1].set_title(f'Gradient Boosting, MSE: {mse_gb:.3f}')
+ax[1][2].set_title(f'Neural Network, MSE: {mse_nn:.3f}')
 fig.supylabel(r'$\mathbb{E}[e^u|X]$')
 
-ax[0][0].set_ylim(1, 5)
-fig.savefig('./figures/continuous_correction_2.pdf', bbox_inches='tight', dpi=300, transparent=True)
+ax[0][0].set_ylim(1, 4)
+fig.savefig('./figures/continuous_correction_independent_squared.pdf', bbox_inches='tight', dpi=300, transparent=True)
